0733d5de9a664a61f2e8183dd31fdc842155853f
Soheil Zaremehrjardi <soheil7100@gmail.com>
2026-01-09 06:51:24 +0100

    Add Fahrzeug (car) class

--- public/index.php
+++ public/index.php
@@ -2,5 +2,9 @@
 
 require_once __DIR__ . '/../config/config.inc.php';
 
+use App\Vehicle\Fahrzeug;
+
+$farhzeug = new Fahrzeug('hufks' , 'PKW');
+print_r($farhzeug);
 
 include_once __DIR__ . '/../templates/template.php';
---
+++ src/App/Vehicle/Fahrzeug.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Vehicle;
+
+class Fahrzeug {
+
+    private String $kennzeichen;
+    private int $parkenStart;
+    private int $parkenEnde;
+    protected $parkNummer;
+    protected string $type;
+
+    public function __construct(string $kennzeichen, string $type, ?int $parkenStart = null, $parkNummer = null)
+    {
+        $this->kennzeichen = strtoupper($kennzeichen);
+        $this->parkenStart = $parkenStart ?? time();
+        $this->type = $type;
+        $this->parkNummer = $parkNummer ?? 123;
+    }
+
+    public function getKennzeichen()
+    {
+        return $this->kennzeichen;
+    }
+
+    public function getParkenStart()
+    {
+        return $this->parkenStart;
+    }
+
+    public function getParkenEnde()
+    {
+        return $this->parkenEnde;
+    }
+
+    public function setParkenEnde($parkenEnde)
+    {
+        $this->parkenEnde = $parkenEnde;
+    }
+
+    public function getParkdauer(): int
+    {
+        return $this->parkenEnde - $this->parkenStart;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getParkNummer()
+    {
+        return $this->parkNummer;
+    }
+}
