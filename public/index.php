185d5e9e9df6ce0e96680b95f363400a016202ca
Soheil Zaremehrjardi <soheil7100@gmail.com>
2026-01-16 11:45:59 +0100

    Add car data from form to list

--- public/index.php
+++ public/index.php
@@ -2,10 +2,32 @@
 
 require_once __DIR__ . '/../config/config.inc.php';
 
-use App\Vehicle\Fahrzeug;
 use App\Vehicle\PKW;
+use App\Parking\Parkplatz;
+use App\Vehicle\Motorrad;
+use App\Vehicle\Transporter;
 
-$farhzeug = new PKW('hufks');
-print_r($farhzeug);
+
+if (!isset($_SESSION['parkplatz'])) {
+    $_SESSION['parkplatz'] = new Parkplatz();
+}
+
+$action = isset($_GET['a']) ? trim($_GET['a']) : null;
+
+switch($action) {
+    case 'add':
+        $kennzeichen = isset($_POST['kennzeichen']) ? trim($_POST['kennzeichen']): null;
+        $typ =  isset($_POST['typ']) ? trim($_POST['typ']): null;
+
+        $f = match(strtolower($typ)) {
+            'pkw' => new Pkw($kennzeichen),
+            'motorrad' => new Motorrad($kennzeichen),
+            'transporter' => new Transporter($kennzeichen)
+        };
+
+        $_SESSION['parkplatz']->add($f);
+        print_r($_SESSION['parkplatz']->getFahrzeuge());
+        break;
+}
 
 include_once __DIR__ . '/../templates/template.php';
